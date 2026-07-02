53c292b65660f0a126b5fe925dbbb1381fff9532
Benjamin Gathuku <benjamingathukumuthoni@gmail.com>
2026-07-01 21:42:13 -0500

    Fix domain config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // TODO: change to your real domain before deploying
-  site: 'site: 'https://fulltimefootball.net',
+  site: 'https://fulltimefootball.net',
   integrations: [sitemap()],
 });
